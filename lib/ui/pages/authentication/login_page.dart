3f831bfde41754713675620514ae6b1246545a82
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-05-02 09:11:39 -0500

    add missing ui

--- lib/ui/pages/authentication/login_page.dart
+++ lib/ui/pages/authentication/login_page.dart
@@ -1,18 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class LoginPage extends StatelessWidget {
+import '../../controllers/authentication_controller.dart';
+import 'signup_page.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  AuthenticationController controller = Get.find<AuthenticationController>();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: const Key('loginScaffold'),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text("App Login"),
-            ElevatedButton(onPressed: () {}, child: Text("Login")),
-            TextButton(onPressed: () {}, child: Text("Create user"))
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const Text(
+                      "Login with email",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      key: const Key('loginEmail'),
+                      controller: _emailController,
+                      decoration: const InputDecoration(labelText: 'Email'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter email";
+                        } else if (!value.contains('@')) {
+                          return "Enter valid email address";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      key: const Key('loginPassord'),
+                      controller: _passwordController,
+                      decoration: const InputDecoration(labelText: "Password"),
+                      keyboardType: TextInputType.number,
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter password";
+                        } else if (value.length < 6) {
+                          return "Password should have at least 6 characters";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        key: const Key('loginSubmit'),
+                        onPressed: () async {
+                          // this line dismiss the keyboard by taking away the focus of the TextFormField and giving it to an unused
+                          FocusScope.of(context).requestFocus(FocusNode());
+                          final form = _formKey.currentState;
+                          form!.save();
+                          if (form.validate()) {
+                            var value = await controller.login(
+                                _emailController.text,
+                                _passwordController.text);
+                            if (value) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(content: Text('User ok')));
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('User problem')));
+                            }
+                          } else {
+                            final snackBar = const SnackBar(
+                              content: Text('Validation nok'),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          }
+                        },
+                        child: const Text("Submit")),
+                  ],
+                ),
+              ),
+            ),
+            TextButton(
+                key: const Key('loginCreateUser'),
+                onPressed: () {
+                  Get.to(() => const SignUpPage());
+                },
+                child: const Text("Create user"))
           ],
         ),
       ),
--- lib/ui/pages/authentication/signup_page.dart
+++ lib/ui/pages/authentication/signup_page.dart
@@ -1,18 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SignUpPage extends StatelessWidget {
+import '../../controllers/authentication_controller.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    AuthenticationController controller = Get.find();
     return Scaffold(
+      key: Key('signUpScaffold'),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text("App SignUp"),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Create user"),
-            )
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const Text(
+                      "Create User",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      key: const Key('signUpEmail'),
+                      controller: _emailController,
+                      decoration: const InputDecoration(labelText: 'Email'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter email";
+                        } else if (!value.contains('@')) {
+                          return "Enter valid email address";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      key: const Key('signUpPassord'),
+                      controller: this._passwordController,
+                      decoration: const InputDecoration(labelText: "Password"),
+                      keyboardType: TextInputType.number,
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter password";
+                        } else if (value.length < 6) {
+                          return "Password should have at least 6 characters";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        key: const Key('signUpSubmit'),
+                        onPressed: () {
+                          // this line dismiss the keyboard by taking away the focus of the TextFormField and giving it to an unused
+                          FocusScope.of(context).requestFocus(FocusNode());
+                          final form = _formKey.currentState;
+                          form!.save();
+                          if (form.validate()) {
+                            controller
+                                .signup(_emailController.text,
+                                    _passwordController.text)
+                                .then((value) {
+                              if (value) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text('User ok')));
+                                Get.back();
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('User problem')));
+                              }
+                            });
+                          } else {
+                            final snackBar = SnackBar(
+                              content: Text('Validation nok'),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          }
+                        },
+                        child: const Text("Submit")),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
