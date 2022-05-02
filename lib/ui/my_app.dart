115132700f39a8bf199d002f6574320dc4626043
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-05-02 09:24:36 -0500

    add missing ui 2

--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'pages/authentication/signup_page.dart';
+import 'controllers/authentication_controller.dart';
+import 'pages/authentication/login_page.dart';
+import 'pages/home/content.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  AuthenticationController authenticationController = Get.find();
+
+  @override
+  initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Authentication Flow',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //home: Content(),
-      home: SignUpPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Authentication Flow',
+        themeMode: ThemeMode.system,
+        home: GetX<AuthenticationController>(
+          builder: (controller) {
+            if (controller.logged) {
+              return Content();
+            }
+            return LoginPage();
+          },
+        ));
   }
 }
--- lib/ui/pages/home/profile_page.dart
+++ lib/ui/pages/home/profile_page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../controllers/authentication_controller.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: ElevatedButton(
-        child: Text('Logout'),
-        onPressed: () {},
-      )),
-    );
+    AuthenticationController controller = Get.find();
+    return Center(
+        child: ElevatedButton(
+      key: const Key('profileLogout'),
+      child: const Text('Logout'),
+      onPressed: () {
+        controller.logout();
+      },
+    ));
   }
 }
