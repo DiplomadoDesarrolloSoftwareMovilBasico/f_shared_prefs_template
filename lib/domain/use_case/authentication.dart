665169e3c7bcf06b26204635b4e81ee6f3f6e11d
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-05-02 09:46:39 -0500

    add comment 2

--- lib/domain/use_case/authentication.dart
+++ lib/domain/use_case/authentication.dart
@@ -4,7 +4,7 @@ class Authentication {
   final _sharedPreferences = LocalPreferences();
 
   //ejemplo para almacenar un string
-  // _sharedPreferences.storeData<String>('user', user);
+  // await_sharedPreferences.storeData<String>('user', user);
 
   // aquí hay un ejemplo de cómo leer un bool
   Future<bool> get init async =>
